3138f9abb9a38338cb66d166c0c0373d47245707
Greg L Gomez <greglgomez@gmail.com>
2020-04-05 19:58:58 +0100

    introduced basic logic

--- js/app.js
+++ js/app.js
@@ -1,55 +1,134 @@
-// initialise primodial materials
-let hydrogen = {
-    name: 'hydrogen',
-    total: 0,
-    symbol: 'H'
-},
-helium = {
-    name: 'helium',
-    total: 0,
-    symbol: 'He'
+// TODO: introduce an element of randomness into the game via cosmic dust with variable elemental content.
+
+// initialise the universe
+let universe = {
+    elements: {
+        hydrogen: {
+            name: 'hydrogen',
+            total: 0,
+            symbol: 'H'
+        },
+        helium: {
+            name: 'helium',
+            total: 0,
+            symbol: 'He'
+        }
+    },
+    measures: {
+        time: {
+            name: 'time',
+            total: 0,
+            symbol: 's'
+        },
+        length: {
+            name: 'length',
+            total: 0,
+            symbol: 'm'
+        },
+        mass: {
+            name: 'mass',
+            total: 0,
+            symbol: 'kg'
+        },
+        temperature: {
+            name: 'temperature',
+            total: 0,
+            symbol: 'K'
+        },
+        pressure: {
+            name: 'pressure',
+            total: 0,
+            symbol: 'P'
+        }
+    },
+    formations: {
+        wisp: {
+            name: 'wisp',
+            requires: {
+                hydrogen: 0
+            }
+        },
+        cloud: {
+            name: 'cloud',
+            requires: {
+                hydrogen: 20
+            }
+        },
+        denseclump: {
+            name: 'dense clump',
+            requires: {
+                hydrogen: 100
+            }
+        }
+    }
 };
 
-// TODO - cosmic dust needs to be more complex and introduce random element into the game by having random composition.
-/*let cosmicdust = {
-    name: 'cosmic dust',
-    total: 0,
-    symbol: 'cd'
-};*/
+/* 
+STEPS TO STAR FORMATION
+1. molecular clouds. pressure > gravity
+2. dense clumps for gas and dust form. gravity > pressure
+3. gains mass, gravity accelerates collapse
+4. Rotation rate increases, creating cirucmstellar disk
+    inner disk feeds protostar
+    remaining disk flattens into protoplanetary disk
+    planets form from the disk
+5. planetary system forms
+    star turns on (H burning in core)
+    wind blows away disk
+    planets sweep out orbits
 
-// initialise base units of the universe
-let time = {
-    name: 'time',
-    total: 0,
-    symbol: 's',
-    description: 'The duration of 9192631770 periods of the radiation corresponding to the transition between the two hyperfine levels of the ground state of the caesium-133 atom.'
-},
-length = {
-    name: 'length',
-    total: 0,
-    symbol: 'm',
-    description: 'The distance travelled by light in vacuum in 1/299792458 second.'
-},
-mass = {
-    name: 'mass',
-    total: 0,
-    symbol: 'kg',
-    description: 'The kilogram is defined by setting the Planck constant h exactly to 6.62607015×10−34 J⋅s (J = kg⋅m2⋅s−2), given the definitions of the metre and the second.'
-},
-temperature = {
-    name: 'temperature',
-    total: 0,
-    symbol: 'K',
-    description: 'The kelvin is defined by setting the fixed numerical value of the Boltzmann constant k to 1.380649×10−23 J⋅K−1, (J = kg⋅m2⋅s−2), given the definition of the kilogram, the metre, and the second.'
+star stages:
+- nebula
+- protostar
+- 'late term' protostars - T Tauri stars - aren't fusing H yet
+    circumstellar disks
+    fast stellar winds
+- pre-main sequence star
+- main sequence star
+*/
+
+// generic clicker function
+function clickElement(number, element) {
+    let targetElement = universe.elements[element]; // address element
+    targetElement.total += number; // increment element total
+    document.getElementById( element ).innerHTML = targetElement.total; // update interface with new total
 };
 
-// initialise derived units
-pressure = {
-    name: 'pressure',
-    total: 0,
-    symbol: 'P',
-    description: ''
-};
+// acquire mass function
+function addMass() {
+    let massCost = Math.floor(10 * Math.pow(1.1,universe.measures.mass.total));
+    if (universe.elements.hydrogen.total >= massCost) {
+        universe.measures.mass.total += 1;
+        universe.elements.hydrogen.total = universe.elements.hydrogen.total - massCost;
+        document.getElementById('hydrogen').innerHTML = universe.elements.hydrogen.total;
+        document.getElementById('mass').innerHTML = universe.measures.mass.total;
+    }
+    let nextCost = Math.floor(10 * Math.pow(1.1,universe.measures.mass.total));
+    document.getElementById('massCost').innerText = `(${nextCost}H)`;
+}
+
+
+
+// functions sandbox
+/* 
+check universe.elements.hydrogen.total
+against universe.formations.[i].requires.hydrogen
+*/
+
+// function checkNextFormation() {
+//     for (let index = 0; index < array.length; index++) {
+//         const number = array[index];
+//         if (universe.elements.hydrogen.total >= universe.formations) {
+            
+//         }
+//     }
+// }
+
+
+
+
+// game loop
+
 
 /* 
 RELATIONSHIP BETWEEN MATERIALS AND UNITS OF MEASURE
@@ -85,31 +164,4 @@ supernovae
 young massive stars
     UV & fast stellar winds blow materials into 'walls'
 */
-
-/* 
-STEPS TO STAR FORMATION
-1. molecular clouds. pressure > gravity
-2. dense clumps for gas and dust form. gravity > pressure
-3. gains mass, gravity accelerates collapse
-4. Rotation rate increases, creating cirucmstellar disk
-    inner disk feeds protostar
-    remaining disk flattens into protoplanetary disk
-    planets form from the disk
-5. planetary system forms
-    star turns on (H burning in core)
-    wind blows away disk
-    planets sweep out orbits
-*/
-
-/* 
-star stages:
-- nebula
-- protostar
-- 'late term' protostars - T Tauri stars - aren't fusing H yet
-    circumstellar disks
-    fast stellar winds
-- pre-main sequence star
-- main sequence star
-*/
-
 
